3b5ed2c43df92fb1d69abc17c60c17a410b67d75
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-14 11:36:32 +0200

    Bash script now includes population size.

---
+++ run_parallel2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /project_antwerp/code/JointAttractorNets_GA
+# pip install wfdb
+python -m pip install -U pip
+pip3 install pandas
+pip install pygad
+pip install brian2
+pip install lmfit
+
+nums=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40)
+size=5
+
+for num in "${nums[@]}"
+do
+    echo "Running script for num: $num"
+    python3 parallel_processing_test2.py "$num" "$size"
+done

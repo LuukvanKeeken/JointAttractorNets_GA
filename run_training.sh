cc4b452b3545a0c9728eeb353971c326fb473a87
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-14 09:33:21 +0200

    Script for running on cluster.

---
+++ run_training.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#script to be run server-side
+
+# install requirements, then execute script with given arguments
+
+cd /project_antwerp/code/JointAttractorNets_GA
+# pip install wfdb
+
+
+
+#for 1 run:
+python3 checknumcpus.py
+

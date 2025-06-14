df52b0648011b7d2f5360eae32673620dc769a84
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-14 11:04:20 +0200

    Smaller population.

--- parallel_processing_test2.py
+++ parallel_processing_test2.py
@@ -58,7 +58,7 @@ else:
 
 
 # GA hyperparameters
-population_size = 200
+population_size = 10
 num_generations = 2
 num_parents_mating = 2
 mutation_type = "random" # Options: "random", "swap", "inversion", "scramble", "adaptive", or a custom function

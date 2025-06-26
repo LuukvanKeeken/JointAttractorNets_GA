c6513952178595e68b83870edd48f4b9b9271112
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-26 19:06:11 +0200

    Checking what the values in lgf and pgf are.

--- GA_optimizer_v2.py
+++ GA_optimizer_v2.py
@@ -96,7 +96,9 @@ def on_generation(ga_instance):
         with open(f'times{population_size}.txt', 'a') as f:
             f.write(f"Processes: {num_processes}, Time: {time.time() - first_generation_end_time:.2f} seconds\n")
     
-    solution, solution_fitness, solution_idx = ga_instance.best_solution(pop_fitness=ga_instance.previous_generation_fitness)
+    print(f"Mean last generation fitness: {np.mean(ga_instance.last_generation_fitness):.4f} +/- {np.std(ga_instance.last_generation_fitness):.4f}")
+    print(f"Mean previous generation fitness: {np.mean(ga_instance.previous_generation_fitness):.4f} +/- {np.std(ga_instance.previous_generation_fitness):.4f}")
+    solution, solution_fitness, solution_idx = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)
     gens_completed = ga_instance.generations_completed
     print(f"Generation {gens_completed} - Best composite error: {1/(solution_fitness)} (sigma_exc: {solution[0]}, sigma_inh: {solution[1]}, g_exc: {solution[2]} mV, g_inh: {solution[3]} mV)")
     print(f"Time elapsed: {time.time() - start_time:.2f} seconds")
@@ -201,7 +203,7 @@ if __name__ == '__main__':
     ga_instance.run()
 
     start_time = time.time()  
-    solution, solution_fitness, solution_idx = ga_instance.best_solution(pop_fitness=ga_instance.previous_generation_fitness)
+    solution, solution_fitness, solution_idx = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)
     print(f"Retrieving best solution took {time.time() - start_time:.2f} seconds")
     print(f"""Best solution found:
     composite error: {1/solution_fitness}

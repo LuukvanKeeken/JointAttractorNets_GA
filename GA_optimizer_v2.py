327520ed64b22d1bc51f93e67fe8e4c874ae4696
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-26 22:47:14 +0200

    Roulette wheel selection, 50 parents mating

--- GA_optimizer_v2.py
+++ GA_optimizer_v2.py
@@ -66,14 +66,14 @@ else:
 
 # GA hyperparameters
 num_generations = 500
-num_parents_mating = 20
-mutation_type = "random" # Options: "random", "swap", "inversion", "scramble", "adaptive", or a custom function
-mutation_num_genes = 1 # Number of genes to mutate in each solution. If using mutation_type="adaptive", this should be a list of 2 numbers: the first for lower-than-average fitness solutions, the second for higher-than-average fitness solutions.
+num_parents_mating = 50
+mutation_type = "adaptive" # Options: "random", "swap", "inversion", "scramble", "adaptive", or a custom function
+mutation_num_genes = [2,1] # Number of genes to mutate in each solution. If using mutation_type="adaptive", this should be a list of 2 numbers: the first for lower-than-average fitness solutions, the second for higher-than-average fitness solutions.
 
-parent_selection_type = "rank" # Options: "sss" (steady state selection), "rws" (roulette wheel selection),
+parent_selection_type = "rws" # Options: "sss" (steady state selection), "rws" (roulette wheel selection),
                               # "sus" (stochastic universal selection), "rank", "tournament", "random", or a custom function
 crossover_type = "single_point" # Options: "single_point", "two_points", "uniform", "scattered", or a custom function
-keep_elitism = 5
+keep_elitism = 1
 
 
 
@@ -100,13 +100,14 @@ def on_generation(ga_instance):
         # Write times to a file
         with open(f'times{population_size}.txt', 'a') as f:
             f.write(f"Processes: {num_processes}, Time: {time.time() - first_generation_end_time:.2f} seconds\n")
-    
-    print(f"Generation mean: {np.mean(ga_instance.last_generation_fitness):.4f} +/- {np.std(ga_instance.last_generation_fitness):.4f}")
+
+    print(f"Generation mean error: {np.mean(1/np.array(ga_instance.last_generation_fitness)):.4f} +/- {np.std(1/np.array(ga_instance.last_generation_fitness)):.4f}")
 
     solution, solution_fitness, solution_idx = ga_instance.best_solution(pop_fitness=ga_instance.last_generation_fitness)
     gens_completed = ga_instance.generations_completed
     with open(f"exp_results.txt", "a") as f:
         f.write(f"Generation {gens_completed} - Best composite error: {1/(solution_fitness)} (sigma_exc: {solution[0]}, sigma_inh: {solution[1]}, g_exc: {solution[2]} mV, g_inh: {solution[3]} mV)\n")
+        f.write(f"Generation mean error: {np.mean(1/np.array(ga_instance.last_generation_fitness)):.4f} +/- {np.std(1/np.array(ga_instance.last_generation_fitness)):.4f}\n")
         f.write(f"Time elapsed: {time.time() - start_time:.2f} seconds\n")
     print(f"Generation {gens_completed} - Best composite error: {1/(solution_fitness)} (sigma_exc: {solution[0]}, sigma_inh: {solution[1]}, g_exc: {solution[2]} mV, g_inh: {solution[3]} mV)")
     print(f"Time elapsed: {time.time() - start_time:.2f} seconds")

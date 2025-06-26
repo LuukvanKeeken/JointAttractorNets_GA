04903471d1b8a97891b384616add241d1c6191f7
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-26 14:14:12 +0200

    Checking how quick the grid search code is.

---
+++ run_training_gridsearch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#script to be run server-side
+
+# install requirements, then execute script with given arguments
+
+cd /project_antwerp/code/JointAttractorNets_GA
+# pip install wfdb
+python -m pip install -U pip
+pip3 install pandas
+pip install pygad
+pip install brian2
+pip install lmfit
+pip install tqdm
+
+
+
+#for 1 run:
+python3 gridSearch_optimizer.py
+

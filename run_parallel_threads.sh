eaebfaff8252aabe9f7e3fcdd7f62103f3c252f7
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-26 13:13:59 +0200

    Trying pygad's threads instead of processes for parallel processing.

---
+++ run_parallel_threads.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /project_antwerp/code/JointAttractorNets_GA
+# pip install wfdb
+python -m pip install -U pip
+pip3 install pandas
+pip install pygad
+pip install brian2
+pip install lmfit
+
+nums=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+size=100
+
+for num in "${nums[@]}"
+do
+    echo "Running script for num: $num"
+    python3 parallel_processing_test4.py "$num" "$size"
+done

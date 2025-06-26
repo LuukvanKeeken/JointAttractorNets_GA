5261acecc96527f54f2bef3fc4a05e76e0d9689b
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-26 17:46:50 +0200

    Trying adaptive mutation

---
+++ run_trainingv2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#script to be run server-side
+
+# install requirements, then execute script with given arguments
+
+cd /project_antwerp/code/JointAttractorNets_GA
+# pip install wfdb
+python -m pip install -U pip
+pip3 install pandas
+pip install pygad
+pip install brian2
+pip install lmfit
+
+
+nums=(16)
+size=100
+
+for num in "${nums[@]}"
+do
+    echo "Running script for num: $num"
+    python3 parallel_processing_test3.py "$num" "$size"
+done
+

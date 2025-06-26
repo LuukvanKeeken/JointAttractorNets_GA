30ce452dd2d0da36845d9676522b5ab356cc8287
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-26 16:00:03 +0200

    One process.

--- gridSearch_optimizer.py
+++ gridSearch_optimizer.py
@@ -179,7 +179,7 @@ if __name__ == '__main__':
     
     # num_proc = mp.cpu_count()   # Adjust the number of worker processes based on your system
     print(f"CPU count: {mp.cpu_count()}")
-    num_proc = 4
+    num_proc = 1
     print(f"Using {num_proc} parallel processes for grid search.")
     results = []
 
@@ -203,7 +203,11 @@ if __name__ == '__main__':
 
     prefs.codegen.target = 'cython'
     print("Brian2 codegen target:", prefs.codegen.target)
-    # print("Brian2 device:", get_device().codegen.target)
+    device = get_device()
+    if hasattr(device, 'codegen'):
+        print("Brian2 device codegen target:", device.codegen.target)
+    else:
+        print("Brian2 device does not have a 'codegen' attribute (likely using RuntimeDevice).")
     
     for i in range(10):
         print()

38216cb6b31206cd38fd53c27f17d0c644ce0d90
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-27 14:16:30 +0200

    Small fix

--- GA_optimizer_v2.py
+++ GA_optimizer_v2.py
@@ -172,7 +172,7 @@ def fitness_func(ga_instance, solution, solution_idx):
         # Check if the composite error is NaN or infinite. In that case, set it to a very high value,
         # so that the fitness value will be very low.
         if np.isnan(composite_error) or np.isinf(composite_error):
-            composite_error = 1e10
+            composite_error = -1
 
     # For now, if there is any exception raised, just give very low fitness value to this solution.
     except Exception as e:

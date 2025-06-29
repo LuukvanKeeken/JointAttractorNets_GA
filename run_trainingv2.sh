6de38e8a15232ea5a9b96e9257a275b8b5a773df
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-06-29 11:14:02 +0200

    Testing out iteration over multiple parameter values

--- run_trainingv2.sh
+++ run_trainingv2.sh
@@ -14,16 +14,22 @@ pip install lmfit
 python -m pip install -U matplotlib
 
 
-nums=(16)
-pop_size=16
-num_parents_mating=8
-mut_prob1=0.25
-mut_prob2=0.1
-num_gens=10
+num_processes=(16 16 16 16 16)
+pop_size=(16 16 16 16 16)
+num_parents_mating=(100 100 100 100 100)
+mut_prob1=(0.1 0.15 0.2 0.5 0.5)
+mut_prob2=(0.05 0.1 0.1 0.3 0.2)
+num_gens=(2 2 2 2 2)
 
-for num in "${nums[@]}"
-do
-    echo "Running script for num: $num"
-    python3 GA_optimizer_v2.py --num_processes "$num" --population_size "$pop_size" --num_parents_mating "$num_parents_mating" --mutation_probability "$mut_prob1" "$mut_prob2" --num_generations "$num_gens"
+for i in "${!num_processes[@]}"; do
+    num_proc="${num_processes[$i]}"
+    pop_size="${pop_size[$i]}"
+    num_parents_mating="${num_parents_mating[$i]}"
+    mut_prob1="${mut_prob1[$i]}"
+    mut_prob2="${mut_prob2[$i]}"
+    num_gen="${num_gens[$i]}"
+
+    echo "Running script for settings: $num_proc processes, $pop_size population, $num_parents_mating parents mating, $mut_prob1 mutation prob1, $mut_prob2 mutation prob2, $num_gen generations"
+    python3 GA_optimizer_v2.py --num_processes "$num_proc" --population_size "$pop_size" --num_parents_mating "$num_parents_mating" --mutation_probability "$mut_prob1" "$mut_prob2" --num_generations "$num_gen"
 done
 

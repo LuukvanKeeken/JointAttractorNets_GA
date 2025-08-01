f1fee5bd7797f025edc55c588d7da82ec53689c4
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-08-01 17:19:57 +0200

    Setting up experiment with fourth error being used

--- run_training_multi.sh
+++ run_training_multi.sh
@@ -15,11 +15,11 @@ python -m pip install -U matplotlib
 
 
 num_processes=(16)
-pop_size=(100)
-num_parents_mating=(50)
+pop_size=(200)
+num_parents_mating=(100)
 mut_prob1=(0.5)
 mut_prob2=(0.3)
-num_gens=(2)
+num_gens=(500)
 
 initial_ranges_mex=(0.05 0.3 0.05 0.3 0.5 1.0 -1.2 -0.3)  # Mexican hat profile ranges
 

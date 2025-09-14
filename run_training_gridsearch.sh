12abd1fc6049c9c402ac0c46547c654171215ed9
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-09-14 13:36:20 +0200

    Use specific versions of packages

--- run_training_gridsearch.sh
+++ run_training_gridsearch.sh
@@ -7,11 +7,11 @@
 cd /project_antwerp/code/temp2/JointAttractorNets_GA
 # pip install wfdb
 python -m pip install -U pip
-pip3 install pandas
-pip install pygad
-pip install brian2
-pip install lmfit
-pip install tqdm
+pip3 install pandas==2.2.3
+pip install pygad==3.4.0
+pip install brian2==2.8.0.4
+pip install lmfit==1.3.3
+pip install tqdm==4.67.1
 
 
 

1887301594fdfa5dfca98955cbce718dc6c7e017
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-09-14 15:09:14 +0200

    wider ranges

--- Optimization Models/gridSearch_optimizer_v3.py
+++ Optimization Models/gridSearch_optimizer_v3.py
@@ -33,8 +33,8 @@ if connectivity_profile == 'mexican_hat':
     # Mexican hat profile parameters
     sigma_exc_range = np.linspace(0.05, 0.25, 10)   # excitatory spread
     sigma_inh_range = np.linspace(0.1, 0.3, 10)    # inhibitory spread
-    g_exc_range   = np.linspace(0.5, 1.0, 10)*mV      # excitatory gain
-    g_inh_range   = np.linspace(-1, -0.3, 10)*mV    # inhibitory gain
+    g_exc_range   = np.linspace(0.01, 1.0, 10)*mV      # excitatory gain
+    g_inh_range   = np.linspace(-1, -0.01, 10)*mV    # inhibitory gain
     
     # Prepare list of parameter combinations (each is a 4-tuple).
     param_grid = list(itertools.product(sigma_exc_range, sigma_inh_range, g_exc_range, g_inh_range))

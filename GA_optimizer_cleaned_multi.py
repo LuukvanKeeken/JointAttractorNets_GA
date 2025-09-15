49a261ff01eb07c68e8ef8c2f110e7257a614cd9
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-09-15 13:47:14 +0200

    Added mV unit

--- GA_optimizer_cleaned_multi.py
+++ GA_optimizer_cleaned_multi.py
@@ -346,7 +346,7 @@ def fitness_func(ga_instance, solution, solution_idx):
         })
     elif connectivity_profile == 'cosine':
         params.update({
-            'g_cosine': solution[0],
+            'g_cosine': solution[0]*mV,
             'glob_inh': True if solution[1] >= 0.5 else False,
             'w_inh': solution[2]
         })

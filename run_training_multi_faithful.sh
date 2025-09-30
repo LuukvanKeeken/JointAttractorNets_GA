89fbc6d2ae0e39a84404b48814bdc5aee13323d8
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-09-30 22:32:27 +0200

    100 pop 100 gens 4 genes newest errors short ranges shorter time

--- run_training_multi_faithful.sh
+++ run_training_multi_faithful.sh
@@ -24,7 +24,7 @@ mut_prob2=(0.3)
 num_gens=(100)
 con_prof="cosine"
 
-initial_ranges_cos=(0.0001 100 -100 0.0 23 100 0.5 100)
+initial_ranges_cos=(0.0001 3 -3 0.0 23 100 0.5 100)
 
 for i in "${!num_processes[@]}"; do
     num_proc="${num_processes[$i]}"

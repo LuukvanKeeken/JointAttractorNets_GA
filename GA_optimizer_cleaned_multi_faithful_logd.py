368de97ada0a02cb76f7a2c2ff79f910dde46452
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-02 15:16:12 +0200

    Now using linear scale for d

--- GA_optimizer_cleaned_multi_faithful_logd.py
+++ GA_optimizer_cleaned_multi_faithful_logd.py
@@ -65,8 +65,8 @@ parser.add_argument('--crossover_type', type=str, default='single_point', choice
 parser.add_argument('--keep_elitism', type=int, default=1, help='Number of best solutions to keep in the next generation.')
 parser.add_argument('--rand_mut_min_val', type=float, default=-0.1, help='Minimum value for random mutation.')
 parser.add_argument('--rand_mut_max_val', type=float, default=0.1, help='Maximum value for random mutation.')
-parser.add_argument('--initial_ranges_cos', type=float, nargs=8, default=[0.0, 100, 0.01, 10, 23, 100, 0.5, 100], help='Initial ranges for Cosine connectivity profile: m_min, m_max, d_min, d_max, Iff_min, Iff_max, tau_s_min, tau_s_max.')
-parser.add_argument('--gene_spaces_cos', type=float, nargs=8, default=[0.0, 100, 0.01, 10, 23, 100, 0.5, 100], help='Cosine gene spaces/limits for the optimization: m_min, m_max, d_min, d_max, Iff_min, Iff_max, tau_s_min, tau_s_max.')
+parser.add_argument('--initial_ranges_cos', type=float, nargs=8, default=[0.0, 100, np.log(0.01), np.log(10), 23, 100, 0.5, 100], help='Initial ranges for Cosine connectivity profile: m_min, m_max, d_min, d_max, Iff_min, Iff_max, tau_s_min, tau_s_max.')
+parser.add_argument('--gene_spaces_cos', type=float, nargs=8, default=[0.0, 100, np.log(0.01), np.log(10), 23, 100, 0.5, 100], help='Cosine gene spaces/limits for the optimization: m_min, m_max, d_min, d_max, Iff_min, Iff_max, tau_s_min, tau_s_max.')
 parser.add_argument('--stim_center', type=float, default=1.571, help='Center of the stimulus for the ring attractor model.')
 parser.add_argument('--stim_width', type=float, default=0.5, help='Width of the stimulus for the ring attractor model.')
 parser.add_argument('--tau', type=float, default=10.0, help='Time constant for the ring attractor model in ms.')
@@ -320,7 +320,7 @@ def on_generation(ga_instance):
     gens_completed = ga_instance.generations_completed
     with open(f"{current_results_dirname}/exp_results.txt", "a") as f:
         if connectivity_profile == 'cosine':
-            f.write(f"Generation {gens_completed} - Best specimen's errors: {1/(solution_fitness) - 1e-8} (m: {solution[0]}, d: {solution[1]}, g_cosine: {np.clip(solution[0] + (solution[4] * solution[1]), m_space['low'], m_space['high'])}, w_inh: {np.clip(-solution[0] + (solution[4] * solution[1]), -m_space['high'], m_space['low'])}, Iff: {solution[2]}, tau_s: {solution[3]}, d_sign: {solution[4]})\n")
+            f.write(f"Generation {gens_completed} - Best specimen's errors: {1/(solution_fitness) - 1e-8} (m: {solution[0]}, log d: {solution[1]}, d: {np.exp(solution[1])}, g_cosine: {np.clip(solution[0] + (solution[4] * np.exp(solution[1])), m_space['low'], m_space['high'])}, w_inh: {np.clip(-solution[0] + (solution[4] * np.exp(solution[1])), -m_space['high'], m_space['low'])}, Iff: {solution[2]}, tau_s: {solution[3]}, d_sign: {solution[4]})\n")
         f.write(f"Generation mean FITNESS: {np.mean(positive_fitnesses):.4f} +/- {np.std(positive_fitnesses):.4f} | {len(positive_fitnesses)} working, {len(negative_fitnesses)} failed solutions\n")
         f.write(f"Generation mean ERROR (working solutions): {mean_errors:.4f} +/- {std_errors:.4f}\n")
         f.write(f"      mean center error: {mean_center_errors:.4f} +/- {std_center_errors:.4f}\n")
@@ -332,7 +332,7 @@ def on_generation(ga_instance):
         f.write(f"Generation mean gene standard deviations: {np.mean(gene_std_devs):.4f}\n")
         f.write(f"Generation time: {time.time() - previous_gen_start_time:.2f} seconds\n")
     if connectivity_profile == 'cosine':
-        print(f"Generation {gens_completed} - Best specimen's errors: {1/(solution_fitness) - 1e-8} (m: {solution[0]}, d: {solution[1]}, g_cosine: {np.clip(solution[0] + (solution[4] * solution[1]), m_space['low'], m_space['high'])}, w_inh: {np.clip(-solution[0] + (solution[4] * solution[1]), -m_space['high'], m_space['low'])}, Iff: {solution[2]}, tau_s: {solution[3]}, d_sign: {solution[4]})")
+        print(f"Generation {gens_completed} - Best specimen's errors: {1/(solution_fitness) - 1e-8} (m: {solution[0]}, log d: {solution[1]}, d: {np.exp(solution[1])}, g_cosine: {np.clip(solution[0] + (solution[4] * np.exp(solution[1])), m_space['low'], m_space['high'])}, w_inh: {np.clip(-solution[0] + (solution[4] * np.exp(solution[1])), -m_space['high'], m_space['low'])}, Iff: {solution[2]}, tau_s: {solution[3]}, d_sign: {solution[4]})")
     print(f"Generation time: {time.time() - previous_gen_start_time:.2f} seconds\n")
 
     previous_gen_start_time = time.time()
@@ -498,9 +498,10 @@ if __name__ == '__main__':
         errors: {1/solution_fitness}
         average error: {np.mean(1/solution_fitness)}
         m: {solution[0]}
-        d: {solution[1]}
-        g_cosine: {np.clip(solution[0] + (solution[4] * solution[1]), m_space['low'], m_space['high'])} mV
-        w_inh: {np.clip(-solution[0] + (solution[4] * solution[1]), -m_space['high'], m_space['low'])} mV
+        log d: {solution[1]}
+        d: {np.exp(solution[1])}
+        g_cosine: {np.clip(solution[0] + (solution[4] * np.exp(solution[1])), m_space['low'], m_space['high'])} mV
+        w_inh: {np.clip(-solution[0] + (solution[4] * np.exp(solution[1])), -m_space['high'], m_space['low'])} mV
         Iff: {solution[2]}
         tau_s: {solution[3]} ms
         d_sign: {solution[4]}
@@ -511,9 +512,10 @@ if __name__ == '__main__':
             f.write(f"errors: {1/solution_fitness}\n")
             f.write(f"average error: {np.mean(1/solution_fitness)}\n")
             f.write(f"m: {solution[0]}\n")
-            f.write(f"d: {solution[1]}\n")
-            f.write(f"g_cosine: {solution[0] + (solution[4] * solution[1])} mV\n")
-            f.write(f"w_inh: {-solution[0] + (solution[4] * solution[1])} mV\n")
+            f.write(f"log d: {solution[1]}\n")
+            f.write(f"d: {np.exp(solution[1])}\n")
+            f.write(f"g_cosine: {solution[0] + (solution[4] * np.exp(solution[1]))} mV\n")
+            f.write(f"w_inh: {-solution[0] + (solution[4] * np.exp(solution[1]))} mV\n")
             f.write(f"Iff: {solution[2]}\n")
             f.write(f"tau_s: {solution[3]} ms\n")
             f.write(f"d_sign: {solution[4]}\n")

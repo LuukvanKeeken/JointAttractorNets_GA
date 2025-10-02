68c2f402170c0a25a5240fdec6e45ec5e70ab792
Luuk van Keeken <luukvankeeken24@gmail.com>
2025-10-02 20:46:53 +0200

    Fix bash script

--- run_training_multi_faithful.sh
+++ run_training_multi_faithful.sh
@@ -35,6 +35,6 @@ for i in "${!num_processes[@]}"; do
 
 
     echo "Running script for settings: $num_proc processes, $pop_size population, $num_parents_mating parents mating, $mut_prob1 mutation prob1, $mut_prob2 mutation prob2, $num_gen generations"
-    python3 GA_optimizer_cleaned_multi_faithful.py --num_processes "$num_proc" --population_size "$pop_size" --num_parents_mating "$num_parents_mating" --mutation_probability "$mut_prob1" "$mut_prob2" --num_generations "$num_gen" --initial_ranges_cos "${initial_ranges_cos[@]}" --connectivity_profile "$con_prof"
+    python3 GA_optimizer_cleaned_multi_faithful.py --num_processes "$num_proc" --population_size "$pop_size" --num_parents_mating "$num_parents_mating" --mutation_probability "$mut_prob1" "$mut_prob2" --num_generations "$num_gen" --connectivity_profile "$con_prof"
 done
 
